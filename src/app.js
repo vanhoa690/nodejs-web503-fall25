80971423b787a53d055dbebee874aa82dc767aba
vanhoa690 <vanhoa690@gmail.com>
2025-09-21 17:49:48 +0700

    Middleware nodejs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import morgan from "morgan";
+
 import router from "./routers";
 
 const app = express();
 
+// Dùng morgan để log ở chế độ 'dev'
+app.use(morgan("dev"));
+
+// Middleware tích hợp để parse JSON: req.body
+app.use(express.json());
+
 app.use("/", router);
-// app.use("/api/v2", router);
 
 app.listen(3000, () => {
   console.log(`Server is running on port http://localhost:3000`);
