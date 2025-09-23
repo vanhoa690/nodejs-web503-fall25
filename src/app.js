b0a2ddb6632a0d33e940e15f6365b613c346444d
vanhoa690 <vanhoa690@gmail.com>
2025-09-23 16:37:48 +0700

    refactor router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import morgan from "morgan";
 
-import router from "./routers";
+import postRouter from "./routers/post";
+import userRouter from "./routers/user";
+import productRouter from "./routers/product";
 
 const app = express();
 
@@ -11,7 +13,16 @@ app.use(morgan("dev"));
 // Middleware tích hợp để parse JSON: req.body
 app.use(express.json());
 
-app.use("/", router);
+// localhost":3000
+app.get("/", (req, res) => {
+  res.send("Hello, chao cac ban: ");
+});
+
+app.use("/api/posts", postRouter);
+
+app.use("/api/users", userRouter);
+
+app.use("/api/products", productRouter);
 
 app.listen(3000, () => {
   console.log(`Server is running on port http://localhost:3000`);
