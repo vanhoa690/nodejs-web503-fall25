641af1d49ae09c1bc6f378c1a5501fa25f43345b
vanhoa690 <vanhoa690@gmail.com>
2025-09-23 16:56:47 +0700

    add post

--- src/routers/post.js
+++ src/routers/post.js
@@ -3,13 +3,19 @@ import { Router } from "express";
 const postRouter = Router();
 
 postRouter.get("/", (req, res) => {
-  console.log(req.query?.name);
-  res.send("Post" + req.query?.name);
+  res.send("Post");
 });
 
-// endpoint: api/posts/greet
+// endpoint: api/posts/detail/:id
 postRouter.get("/detail/:id", (req, res) => {
   res.send("Post detail co id la: " + req.params?.id);
+});
+
+// /api/posts
+postRouter.post("/", (req, res) => {
+  // body: title
+  console.log(req.body);
+  res.json({ body: req.body });
 });
 
 export default postRouter;
