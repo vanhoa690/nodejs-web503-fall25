50f8962fca12094d323553ddd21ee0ed69660430
vanhoa690 <vanhoa690@gmail.com>
2025-09-25 15:18:57 +0700

    add flow api

--- src/routers/post.js
+++ src/routers/post.js
@@ -4,6 +4,8 @@ const postRouter = Router();
 let posts = [
   { id: 1, title: "Bài viết 1", content: "Nội dung bài viết 1" },
   { id: 2, title: "Bài viết 2", content: "Nội dung bài viết 2" },
+  { id: 3, title: "Bài viết 2", content: "Nội dung bài viết 2" },
+  { id: 4, title: "Bài viết 2", content: "Nội dung bài viết 2" },
 ];
 
 // GET /api/posts - Lấy danh sách bài viết
@@ -29,6 +31,8 @@ postRouter.post("/", (req, res) => {
 // DELETE /api/posts/:id - Xóa bài viết
 postRouter.delete("/:id", (req, res) => {
   const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
+  console.log("index", index);
+
   if (index === -1) return res.status(404).json({ error: "Post not found" });
 
   posts.splice(index, 1);
@@ -37,6 +41,8 @@ postRouter.delete("/:id", (req, res) => {
 
 // PUT /api/posts/:id - Cập nhật bài viết
 postRouter.put("/:id", (req, res) => {
+  console.log(typeof req.params.id);
+
   const post = posts.find((p) => p.id === parseInt(req.params.id));
   if (!post) return res.status(404).json({ error: "Post not found" });
 
