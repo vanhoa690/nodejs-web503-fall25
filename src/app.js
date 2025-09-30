f1e6ef0732b46c8a6543e8b627c2351216e37df7
vanhoa690 <vanhoa690@gmail.com>
2025-09-30 16:52:46 +0700

    connect db

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 
 import postRouter from "./routers/post";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -10,6 +11,11 @@ app.use(morgan("dev"));
 
 // Middleware tích hợp để parse JSON: req.body
 app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/nodejs")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB:", err));
 
 // localhost":3000
 app.get("/", (req, res) => {
