a3a5beb394167d7d94b1b027dccac83f3211d52b
vanhoa690 <vanhoa690@gmail.com>
2025-10-04 16:40:19 +0700

    validate joi

---
+++ src/validates/joi-explame.js
@@ -0,0 +1,24 @@
+import Joi from "joi";
+
+const schema = Joi.object({
+  name: Joi.string().required(),
+  age: Joi.number().required().min(0).messages({
+    "number.min": "Age không được âm",
+  }),
+});
+
+const data = { name: 11, age: -1 };
+
+const { error, value } = schema.validate(data, { abortEarly: false }); //value: data, error: thong bao loi: ko loi -> undefined, error.details
+
+if (error) {
+  //error.details : Array ({ message, path, type, context})
+  console.log(
+    "Dữ liệu không hợp lệ:",
+    error.details.map((err) => err.message)
+  ); // Nếu có lỗi, in chi tiết lỗi ra console
+} else {
+  console.log("Dữ liệu hợp lệ:", value); // Nếu dữ liệu hợp lệ, in dữ liệu đã được validate
+}
+
+// ----------------------------------
